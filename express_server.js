ab4502503fdb5e52abd03df1d6c1b0b62b91ddd2
‘Gonzo <gonzonieto@gmail.com>
2022-03-22 20:31:40 +0000

    changed double quotes to single quotes and added route for login with cookie logic

--- express_server.js
+++ express_server.js
@@ -4,8 +4,14 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
-const bodyParser = require("body-parser");
+// using body parsing and cookie parsing middleware from Express
+// http://expressjs.com/en/resources/middleware/body-parser.html
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+
+// http://expressjs.com/en/resources/middleware/cookie-parser.html
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 const generateRandomString = () => Math.random().toString(36).slice(2, 8);
 
@@ -22,7 +28,7 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
@@ -31,15 +37,15 @@ app.get("/u/:shortURL", (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
 
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -70,6 +76,13 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const username  = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyURL app listening on port ${PORT}!`);
 });
