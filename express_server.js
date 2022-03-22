3076c4ef0a68e15219244cf6540ad3b62a47cc9c
‘Gonzo <gonzonieto@gmail.com>
2022-03-22 03:39:53 +0000

    simplify /urls/:shortURL route to avoid getter and use property value syntax

--- express_server.js
+++ express_server.js
@@ -23,11 +23,10 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
   const templateVars = {
-    shortURL: req.params.shortURL,
-    get longURL() {
-      return urlDatabase[this.shortURL];
-    }
+    shortURL,
+    longURL: urlDatabase[shortURL]
   };
   res.render('urls_show', templateVars);
 });
